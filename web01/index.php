b275d720de6e493f2abfec9c7ea7b9750fabd4ae
Pablo González <pablogontroya@gmail.com>
2021-10-20 13:54:49 +0200

    Primer PHP

--- web01/index.php
+++ web01/index.php
@@ -6,5 +6,13 @@
     <body>
         <h1>Esto es un título</h1>
         <p>Esto es un párrafo</p>
+
+        <pre>
+            Ejecuta php -S localhost:9000
+            visital con tu navegador
+        </pre>
+        <?php
+            echo "Esto lo escribe PHP"
+        ?>
     </body>
 </html>
